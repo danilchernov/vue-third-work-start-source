982805355bcd5fe12808b5968733616593595a39
Danil Chernov <d.chernov.personal@gmail.com>
2023-01-24 08:29:58 +0700

    feat: interaction within the counter

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="counter">
+    <CounterButton @action="decrement">-</CounterButton>
+
+    <CounterValue :value="count" />
+
+    <CounterButton @action="increment">+</CounterButton>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+import CounterButton from './CounterButton.vue';
+import CounterValue from './CounterValue.vue';
+
+const count = ref(0)
+const decrement = () => count.value--;
+const increment = () => count.value++;
+
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,7 +1,12 @@
 export default [
 	{
-		name: "Counter",
+		name: "Counter-0",
 		path: "/counter-0",
 		component: () => import("./demo0/DemoCounter.vue"),
+	},
+	{
+		name: "Counter-1",
+		path: "/counter-1",
+		component: () => import("./demo1/DemoCounter.vue"),
 	},
 ];
