ceef9d6f6be8b32d15b436122793d01da5cf2851
Danil Chernov <d.chernov.personal@gmail.com>
2023-02-15 22:27:02 +0700

    feat: Демо 4

---
+++ demo/src/demo4/DemoCounter.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="counter">
+		<CounterButton @action="counterStore.decrement" :disabled="counterStore.isDecrementDisabled">-</CounterButton>
+
+		<CounterValue :value="counterStore.value" />
+
+		<CounterButton @action="counterStore.increment">+</CounterButton>
+	</div>
+</template>
+
+<script setup>
+import { useCounterStore } from "./store";
+
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "./CounterValue.vue";
+
+const counterStore = useCounterStore();
+</script>
+
+<style lang="scss" scoped>
+.counter {
+	display: flex;
+	align-items: center;
+}
+</style>
