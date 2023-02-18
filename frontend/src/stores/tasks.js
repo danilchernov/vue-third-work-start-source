95a46b840369eba9ceafda40ff9b681645fbeb4c
Danil Chernov <d.chernov.personal@gmail.com>
2023-02-18 22:46:01 +0700

    feat: Добавление геттеров хранилища в проект Vue-Work

--- frontend/src/stores/tasks.js
+++ frontend/src/stores/tasks.js
@@ -1,9 +1,60 @@
 import { defineStore } from "pinia";
 
+import { useFiltersStore } from "@/stores/filters";
+import { useUsersStore } from "@/stores/users";
+
 export const useTasksStore = defineStore("tasks", {
   state: () => ({
     tasks: [],
+    filteredTasks: [],
   }),
-  getters: {},
+  getters: {
+    filteredTasks: (state) => {
+      const filtersStore = useFiltersStore();
+
+      const filtersAreEmpty = Object.values(filtersStore.filters).every(
+        (value) => !value.length
+      );
+
+      if (filtersAreEmpty) {
+        return state.tasks;
+      }
+
+      const searchFilter = (task) =>
+        task.title
+          .toLowerCase()
+          .includes(filtersStore.filters.search.toLowerCase().trim());
+
+      const usersFilter = (task) =>
+        filtersStore.filters.users.some((userId) => userId === task.userId);
+
+      const statusesFilter = (task) =>
+        filtersStore.filters.statuses.some(
+          (el) => el === task.status || el === task.timeStatus
+        );
+
+      return state.tasks.filter((task) => {
+        let result = {
+          search: searchFilter,
+          users: usersFilter,
+          statuses: statusesFilter,
+        };
+
+        return Object.entries(result).every(
+          ([key, callback]) =>
+            !filtersStore.filters[key].length || callback(task)
+        );
+      });
+    },
+    getTaskUserById: () => (id) => {
+      const usersStore = useUsersStore();
+      return usersStore.users.find((user) => user.id === id);
+    },
+    sidebarTasks: (state) => {
+      return state.filteredTasks
+        .filter((task) => !task.columnId)
+        .sort((a, b) => a.sortOrder - b.sortOrder);
+    },
+  },
   actions: {},
 });
