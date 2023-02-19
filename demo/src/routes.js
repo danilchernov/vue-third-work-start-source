fad4d7373c29256a1ae1543ac23b2ba912a34289
Danil Chernov <d.chernov.personal@gmail.com>
2023-02-19 12:46:43 +0700

    feat: Создаём подключение к серверу

--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,17 +1,22 @@
 export default [
 	{
 		name: "Counter-0",
-		path: "/counter-0",
+		path: "/demo-0",
 		component: () => import("./demo0/DemoCounter.vue"),
 	},
 	{
 		name: "Counter-1",
-		path: "/counter-1",
+		path: "/demo-1",
 		component: () => import("./demo1/DemoCounter.vue"),
 	},
 	{
 		name: "Counter-4",
-		path: "/counter-4",
+		path: "/demo-4",
 		component: () => import("./demo4/DemoCounter.vue"),
+	},
+	{
+		name: "Server",
+		path: "/demo-5",
+		component: () => import("./demo5/Server.vue"),
 	},
 ];
